003f11eae7dd6c0d76b684a1c7b671f53712e1ff
andrejra <andrejrad@outlook.com>
2015-03-31 01:10:20 +0200

    Dodata nova klasa Menjacnica koja implementira prethodno definisane metode iz Interfejsa Menjacnice

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,26 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejsMenjacnica.interfejsMenjacnica;
+
+public class Menjacnica implements interfejsMenjacnica {
+
+	public void dodajKurs(GregorianCalendar datum, String naziv,
+			String skraceniNaziv, double prodajniKurs, double srednjiKurs,
+			double kupovniKurs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void obrisiKurs(GregorianCalendar datum, String naziv) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
