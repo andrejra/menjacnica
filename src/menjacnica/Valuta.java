77df36d12c1622c64765083a3e5f8ffa8579da64
andrejra <andrejrad@outlook.com>
2015-03-31 00:01:34 +0200

    Dodata klasa Valuta, sa svojim atributima i odgovarajucim geterima i seterima.

---
+++ src/menjacnica/Valuta.java
@@ -0,0 +1,43 @@
+package menjacnica;
+
+public class Valuta {
+
+	String naziv;
+	String skraceniNaziv;
+	double prodajniKurs;
+	double srednjiKurs;
+	double kupovniKurs;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	
+	
+}
